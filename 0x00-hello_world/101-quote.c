68152bfc07f7fb822e8b19149d2a96adf28f89ea
Daniel German <danielchuksgerman@gmail.com>
2023-07-13 06:27:27 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char Mystr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	printf("%s\n", Mystr);
+
+	return (1);
+}
