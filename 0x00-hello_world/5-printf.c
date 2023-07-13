78d85884f50032652d6ecc600c5e273646820b54
Daniel German <danielchuksgerman@gmail.com>
2023-07-13 05:24:33 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints some string using printf'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Mystr[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", Mystr);
+
+	return (0);
+}
