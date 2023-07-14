bc8c36aff0c0a0ee121cbfa38827db64ce37e083
Daniel German <danielchuksgerman@gmail.com>
2023-07-14 03:22:44 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+# include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints the lowercase alphabets followed by new line'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		putchar('\n');
+		letter++;
+	}
+
+	return (0);
+}
