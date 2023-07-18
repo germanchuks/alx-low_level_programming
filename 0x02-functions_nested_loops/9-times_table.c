bd219f6fcbe3cfaeec512ef25d358c905cc927bd
Daniel German <danielchuksgerman@gmail.com>
2023-07-18 06:25:35 -0700

    9 times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * times_table - Entry point
+ *
+ * Description: 'Prints 9 times table'
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int i, j;
+
+	i=0;
+
+	while (i <= 9)
+	{
+		j = 0;
+		
+		while (j <= 9)
+		{
+			int mult = j*i;
+			if (mult >= 10)
+			{
+				_putchar((mult / 10) + '0');
+				_putchar((mult % 10) + '0');
+			} else 
+			{
+				_putchar(mult + '0');
+			}
+
+			if (j!=9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			} else {
+				_putchar('$');
+			}
+
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
