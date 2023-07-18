c6bb876f27450df38ff183334f8b0c754cca0d5b
Daniel German <danielchuksgerman@gmail.com>
2023-07-18 04:54:39 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - Entry point
+ *
+ * Description: 'Computes absolute value of an integer'
+ * @val: 'Integer to be computed'
+ * Return: Always 0 (Success)
+ */
+int _abs(int val)
+{
+	return (abs(val));
+}
