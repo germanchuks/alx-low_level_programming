2da145d2ba6ce313496d9cdc3c72375b7f4561ac
Daniel German <danielchuksgerman@gmail.com>
2023-07-18 02:47:03 -0700

    10x alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
 #include <string.h>
-
+#include "main.h"
 /**
  * main - Entry point
  *
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: 'Print alphabets in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
