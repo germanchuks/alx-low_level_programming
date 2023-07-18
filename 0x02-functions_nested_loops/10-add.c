8f61a80dbfb7c9b15f236b6f0e9c99ce70f03ad7
Daniel German <danielchuksgerman@gmail.com>
2023-07-18 06:46:09 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * Description: 'Adds two integers'
+ * @a: First integer to be summed
+ * @b: Second integer to be summed
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
