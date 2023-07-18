2aa6cb2b4a3ca9d7fefa91b917c530a715146fa1
Daniel German <danielchuksgerman@gmail.com>
2023-07-18 06:35:34 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,32 +10,33 @@ void times_table(void)
 {
 	int i, j;
 
-	i=0;
+	i = 0;
 
 	while (i <= 9)
 	{
 		j = 0;
-		
+
 		while (j <= 9)
 		{
-			int mult = j*i;
+			int mult = j * i;
+
 			if (mult >= 10)
 			{
 				_putchar((mult / 10) + '0');
 				_putchar((mult % 10) + '0');
-			} else 
+			} else
 			{
 				_putchar(mult + '0');
+				if (j != 0)
+				{
+					_putchar(' ');
+				}
 			}
-
-			if (j!=9)
+			if (j != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
-			} else {
-				_putchar('$');
 			}
-
 			j++;
 		}
 		_putchar('\n');
