f8e712462bf28c2ae8c0c0d34164a3ef99d864ab
Daniel German <danielchuksgerman@gmail.com>
2023-07-20 04:18:04 -0700

    Print integers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * print_number - Enry point
+ * Description: 'Prints an integer'
+ * @n: Diagonal count
+ * Return: Returns 0
+ */
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	int revNum = 0;
+	int count = 0;
+	int tempNum = n;
+
+	while (tempNum > 0)
+	{
+		tempNum /= 10;
+		count++;
+	}
+
+	/*Reverse the digits*/
+	while (count > 0)
+	{
+		int digit = n % 10;
+
+		revNum = revNum * 10 + digit;
+		n /= 10;
+		count--;
+	}
+
+	while (revNum > 0)
+	{
+		int digit = revNum % 10;
+
+		_putchar(digit + '0');
+		revNum /= 10;
+	}
+}
