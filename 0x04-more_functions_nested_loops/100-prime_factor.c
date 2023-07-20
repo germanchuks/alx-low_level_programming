ea88d4611ff22cde742f346329a8439546edfee1
Daniel German <danielchuksgerman@gmail.com>
2023-07-20 06:00:25 -0700

    Prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 /**
  * main - Enry point
  *
@@ -8,7 +9,9 @@
  */
 int main(void)
 {
-	unsigned long i, max = -1, num = 612852475143;
+	long int i;
+	long int max = -1;
+	long int num = 612852475143;
 
 	while (num % 2 == 0)
 	{
@@ -16,7 +19,7 @@ int main(void)
 		num /= 2;
 	}
 
-	for (i = 3; i <= sqrt(n); i += 2)
+	for (i = 3; i <= sqrt(num); i += 2)
 	{
 		while (num % i == 0)
 		{
@@ -27,6 +30,6 @@ int main(void)
 	if (num > 2)
 		max = num;
 
-	printf("%lu\n", max);
+	printf("%ld\n", max);
 	return (0);
 }
