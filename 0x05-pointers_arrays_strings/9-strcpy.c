5ce0c3c0e7c63dc84068a8aa2319ab9c2500dac5
Daniel German <danielchuksgerman@gmail.com>
2023-07-24 09:33:03 -0700

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy - Copies the string
+ *
+ * @dest: Destination array
+ * @src: String source
+ * Return: Nothing.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+}
