6c1b0453d83806c6d1dfb219d5ad17e578335d0b
Daniel German <danielchuksgerman@gmail.com>
2023-07-26 00:49:35 -0700

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Destination string
+ * @src: Appending string
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
