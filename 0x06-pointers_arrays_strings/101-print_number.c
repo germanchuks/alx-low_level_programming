fd736fcdef36c151a42409c61f66111beba64a48
Daniel German <danielchuksgerman@gmail.com>
2023-07-26 10:09:03 -0700

    Print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+/**
+ * print_number - Prints an integer
+ * @n: Number
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
