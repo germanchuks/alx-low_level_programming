c15cd5f855965881f46f2940784420c98f0c0ccf
Daniel German <danielchuksgerman@gmail.com>
2023-07-27 05:55:01 -0700

    ROT13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,23 +1,26 @@
-#include "main.h"
 /**
- * rot13 - Encodes strings using rot13
- * @n: String
- * Return: encoded string
+ * rot13 - check the code for Holberton School students.
+ *@n : variable
+ * Return: return
  */
+
 char *rot13(char *n)
 {
-	int i, j;
-	char *str_letters = "abcdefghijklmnopqrstuvwxyzABCDEDGHIJKLMNOPQRSTUVWXYZ";
-	char *rot_letters = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEDGHIJKLM";
+	int i;
+	int a;
+	int d;
+	char b[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char c[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; str_letters[j] != '\0'; j++)
+		d = 1;
+		for (a = 0; a < 52; a++)
 		{
-			if (n[i] == str_letters[j])
+			if (n[i] == b[a] && d == 1)
 			{
-				n[i] = rot_letters[j];
-				break;
+				n[i] = c[a];
+				d = 0;
 			}
 		}
 	}
