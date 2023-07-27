40511926dfcca677ce90b65af75dde939c4ae9d6
Daniel German <danielchuksgerman@gmail.com>
2023-07-27 10:45:38 -0700

    PRint buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,46 +1,76 @@
 #include <stdio.h>
 /**
- * print_buffer - Prints a buffer
+ * printHex - Converts string to hexadecimal
+ * @b: string
+ * @start: Start position
+ * @end: End position
+ * Return: Nothing
+ */
+void printHex(char *b, int start, int end)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i < end)
+			print("%02x", *(b + start + i));
+		else
+			printf(" ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
+}
+
+/**
+ * isPrintableASCII - Checks if a char is printable as ASCII
+ * @n: Integer
+ * Return: 1 if true, 0 if false
+ */
+int isPrintableASCII(int n)
+{
+	return (n >= 32 && n <= 126);
+}
+
+/**
+ * printASCII: Converts string to ASCII, replace non printable
  * @b: String
- * @size: Size of string
+ * @start: Start position
+ * @end: End position
+ */
+void printASCII(char *b, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!isPrintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
+}
+
+/**
+ * print_buffer: Prints a buffer
+ * @b: String
+ * @size: Size of buffer
  * Return: Nothing
  */
 void print_buffer(char *b, int size)
 {
-	int count = (size / 10) + 1, i = 0;
+	int start, end;
 
 	if (size > 0)
 	{
-		while (i < count)
+		for (start = 0; start < size; start += 10)
 		{
-			int j = i * 10, k = i * 10;
-
-			printf("%08x:", i * 10);
-			while (j < i * 10 + 10)
-			{
-				if (j == size)
-				{
-					printf("%10s", " ");
-					break;
-				}
-				if (j % 2 == 0)
-					printf(" ");
-				printf("%02x", (unsigned char)b[j]);
-				j++;
-			}
-			printf(" ");
-			while (k < i * 10 + 10)
-			{
-				if (k == size)
-					break;
-				if ((b[k] >= 0 && b[k] <= 7) || b[k] == '\n' || b[k] == '\0')
-					printf(".");
-				else
-					printf("%c", b[k]);
-				k++;
-			}
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHex(b, start, end);
+			printASCII(b, start, end);
 			printf("\n");
-			i++;
 		}
 	}
 	else
