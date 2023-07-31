b0854bddf49a2b24102718dfdfbd230c6a71aa5c
Daniel German <danielchuksgerman@gmail.com>
2023-07-31 02:00:22 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include <string.h>
 /**
  * _strchr - Locates a character in a string.
- * @s: String to be checked.
+ * @s: Pointer to string to be checked.
  * @c: Character to be found in string.
  *
  * Return: Pointer to first occurence if character is found, NULL if not found
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: Pointer to string to be searched.
+ * @accept: Pointer to character(s) to match against.
+ *
+ * Return: Pointer to first occurence if character is found, NULL if not found
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
