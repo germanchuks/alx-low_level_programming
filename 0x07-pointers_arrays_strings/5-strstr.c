89b4a8fa150ccf5e099d30719112a4c4ecefac33
Daniel German <danielchuksgerman@gmail.com>
2023-07-31 02:21:35 -0700

    5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+ * _strstr - Finds the first occurrence of the substring in a string.
+ * @haystack: Pointer to string to be searched.
+ * @needle: Pointer to substring to match against.
+ *
+ * Return: Pointer to beginning of the located substring, or NULL if not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
