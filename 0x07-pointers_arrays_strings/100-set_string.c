1d132785fd3bc5ee465c4a9b15c1022cfd922f8f
Daniel German <danielchuksgerman@gmail.com>
2023-07-31 04:06:44 -0700

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+/**
+ * set_string - Prints the sum of two diagonals of a square matrix of integers.
+ * @s: Pointer to string to convert from
+ * @to: Pointer to string to be converted
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
