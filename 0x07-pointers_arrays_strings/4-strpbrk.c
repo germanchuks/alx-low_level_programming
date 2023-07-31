52f4d6da14aaebf2ed5e9d79239c54c88bb67fa1
Daniel German <danielchuksgerman@gmail.com>
2023-07-31 02:14:46 -0700

    4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include <string.h>
+/**
+ * _strpbrk - Gets the length of a prefix substring.
+ * @s: Pointer to string to be searched.
+ * @accept: Pointer to character(s) to match against.
+ *
+ * Return: Pointer to byte in s that matches one of bytes in accept,
+ * NULL if none is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
