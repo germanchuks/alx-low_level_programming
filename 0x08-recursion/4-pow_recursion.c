9a354f9b78b9d6595642c17d3341470e28c56123
Daniel German <danielchuksgerman@gmail.com>
2023-08-02 02:24:24 -0700

    4-pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  * @x: Base
  * @y: Exponent
  *
- * Return: value of x raised to power of y, or -1 if y is lower than 0.
+ * Return: value of x to power of y, or -1 if y is lower than 0.
  */
 int _pow_recursion(int x, int y)
 {
@@ -11,6 +11,5 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	if (y > 0)
-		return (x * _pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
