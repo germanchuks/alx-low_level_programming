0e820018f9947b1bf7ca4fd9cfbf92d6fcc473d7
Daniel German <danielchuksgerman@gmail.com>
2023-08-02 00:43:11 -0700

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: String to check length
+ *
+ * Return: Length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
