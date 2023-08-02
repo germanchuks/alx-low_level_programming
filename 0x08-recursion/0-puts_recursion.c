47b914ae4f18111a9c6657e900b75031654db3a6
Daniel German <danielchuksgerman@gmail.com>
2023-08-02 00:07:07 -0700

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - Prints a string, followed by a new line
+ * @s: String to print
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
