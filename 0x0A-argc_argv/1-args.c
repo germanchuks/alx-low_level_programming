722c97c2e043de8dccb432a8d7367f197a88a997
Daniel German <danielchuksgerman@gmail.com>
2023-08-04 06:59:08 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the program name followed by a new line
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * 
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
