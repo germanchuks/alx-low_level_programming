8e49b5fcc7a967f85b8bc431c5eda62e4b88124f
Daniel German <danielchuksgerman@gmail.com>
2023-08-04 07:32:40 -0700

    3-mul fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,15 +9,16 @@
  */
 int main(int argc, char **argv)
 {
-	int result;
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	int result, a, b;
 
-	if (argc < 3 || argc > 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 
 	result = a * b;
 	printf("%d\n", result);
