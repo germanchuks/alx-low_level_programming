cebea26ce91e4f16c9dabd5131b23f9f56f63ca2
Daniel German <danielchuksgerman@gmail.com>
2023-08-14 11:31:04 -0700

    5-free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_dog - Frees memory allocated to struct dog.
+ * @d: Pointer to struct dog.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
