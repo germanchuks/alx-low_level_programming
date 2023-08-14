4198d3c4eec6b4f6f740a885a6e27140e5332193
Daniel German <danielchuksgerman@gmail.com>
2023-08-14 08:49:54 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Shows dogs data.
+ * @name: Name of dog.
+ * @age: Age of dog.
+ * @owner: Owner of dog.
+ *
+ * Description:
+ * This structure shows data for a dog. 'Name' is a pointer of type char and
+ * holds the name of the dog as string. 'Age' is of type float and holds the
+ * age of the dog. 'Owner' is a pointer of type char and stores the name of the
+ * owner as string.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
