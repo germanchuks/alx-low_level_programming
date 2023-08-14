325df4affb2cad4eed07e2d811b78a7823227112
Daniel German <danielchuksgerman@gmail.com>
2023-08-14 10:18:16 -0700

    dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,12 +13,12 @@
  * age of the dog. 'Owner' is a pointer of type char and stores the name of the
  * owner as string.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
