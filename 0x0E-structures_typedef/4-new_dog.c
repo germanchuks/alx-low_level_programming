a8e415d7dde7e7e73d37f4740d75254465cdcb4f
Daniel German <danielchuksgerman@gmail.com>
2023-08-14 11:02:49 -0700

    4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+
+	if (new_dog == NULL || name == NULL || owner == NULL)
 		return (NULL);
 
 	new_dog->name = strdup(name);
