99b5b1c0f994a933a84967dc05ea6034dc6b81c3
Daniel German <danielchuksgerman@gmail.com>
2023-08-14 11:22:09 -0700

    4-new_dog fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,28 +12,43 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-
-	new_dog = malloc(sizeof(struct dog));
+	int name_len = 0;
+	int owner_len = 0;
+	int i;
+	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL || name == NULL || owner == NULL)
 		return (NULL);
+	while (name[name_len] != '\0')
+		name_len++;
+	new_dog->name = malloc(sizeof(char) * (name_len + 1));
 
-	new_dog->name = strdup(name);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
-	new_dog->age = age;
-	new_dog->owner = strdup(owner);
+	while (owner[owner_len] != '\0')
+		owner_len++;
+	new_dog->owner = malloc(sizeof(char) * (owner_len + 1));
 	if (new_dog->owner == NULL)
 	{
-		free(new_dog);
 		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
-
+	i = 0;
+	while (i <= name_len)
+	{
+		new_dog->name[i] = name[i];
+		i++;
+	}
+	i = 0;
+	while (i <= owner_len)
+	{
+		new_dog->owner[i] = owner[i];
+		i++;
+	}
+	new_dog->age = age;
 	return (new_dog);
 }
