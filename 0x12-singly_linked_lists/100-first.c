75a142c11e005f8bb955a0b908ce70fb26544f64
Daniel German <danielchuksgerman@gmail.com>
2023-08-24 11:56:08 -0700

    Fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,6 @@
-#include "main.h"
+#include "lists.h"
+
+void initialStatement(void) __attribute__((constructor));
 
 /**
  * initialStatement - Prints some sentence before the main function is executed
@@ -7,6 +9,6 @@
 */
 void initialStatement(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-		I bore my house upon my back!\n");
+        printf("You're beat! and yet, you must allow,\n");
+        printf("I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_note - Adds a new node at the beginning of a list_t list.
+ * add_node - Adds a new node at the beginning of a list_t list.
  * @head: Double pointer to list
  * @str: String to add to new node.
  *
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	while (str[length])
 		length++;
-	
+
 	newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
 		return (NULL);
