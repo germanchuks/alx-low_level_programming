dc1a59e16389d5ff130fc1a5022ffdf067d78972
Daniel German <danielchuksgerman@gmail.com>
2023-08-24 03:55:19 -0700

    100-first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * initialStatement - Prints some sentence before the main function is executed
+ *
+ * Return: Nothing
+*/
+void initialStatement(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
+}
