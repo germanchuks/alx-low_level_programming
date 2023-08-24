e5c989c4c4b87ebcc8e76207b1f9f4e82567c201
Daniel German <danielchuksgerman@gmail.com>
2023-08-24 12:02:07 -0700

    Fix2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,9 +14,13 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		count++;
 	}
