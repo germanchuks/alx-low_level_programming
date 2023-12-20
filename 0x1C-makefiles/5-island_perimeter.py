e8803f7a402a43d723c338d04bcf4b395a3b5188
Daniel German <danielchuksgerman@gmail.com>
2023-12-20 07:05:18 -0800

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Computes and returns the perimeter of an island"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+
+    perimeter = 0
+    height = len(grid)
+    width = len(grid[0])
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+
+                if i == height - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+
+                if j == width - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return (perimeter)
