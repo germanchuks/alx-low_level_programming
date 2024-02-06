3d494ba13166ebfa64b6af05aadca10e987dfbf3
Daniel German <danielchuksgerman@gmail.com>
2024-02-06 08:42:49 -0800

    Linear search - fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,7 +19,8 @@ int linear_search(int *array, size_t size, int value)
 
 	while (index < size)
 	{
-		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+		printf("Value checked array[%ld] = [%d]\n",
+				index, array[index]);
 
 		if (array[index] == value)
 			return (index);
